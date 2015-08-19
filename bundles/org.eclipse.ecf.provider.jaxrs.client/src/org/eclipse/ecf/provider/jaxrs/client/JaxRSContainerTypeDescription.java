c5fef4e2a6b1075cb26b02fde158ac610afe60ee
slewis <slewis@composent.com>
2015-08-19 15:45:29 -0700

    Simplification of provider impl

--- bundles/org.eclipse.ecf.provider.jaxrs.client/src/org/eclipse/ecf/provider/jaxrs/client/JaxRSContainerTypeDescription.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.ecf.provider.jaxrs.client;
-
-import org.eclipse.ecf.core.ContainerTypeDescription;
-
-public class JaxRSContainerTypeDescription extends ContainerTypeDescription {
-	public JaxRSContainerTypeDescription(String name) {
-		super(name, new JaxRSClientContainerInstantiator(), "JaxRS Container '"+name+"'");
-	}
-}
---
+++ bundles/org.eclipse.ecf.provider.jaxrs.client/src/org/eclipse/ecf/provider/jaxrs/client/JaxRSDistributionProvider.java
@@ -0,0 +1,12 @@
+package org.eclipse.ecf.provider.jaxrs.client;
+
+import org.eclipse.ecf.remoteservice.provider.RemoteServiceDistributionProvider;
+
+public class JaxRSDistributionProvider extends RemoteServiceDistributionProvider {
+
+	public JaxRSDistributionProvider(String name) {
+		super(name, new JaxRSClientContainerInstantiator());
+		setDescription("JaxRS Distribution Provider '"+name+"'");
+	}
+
+}
