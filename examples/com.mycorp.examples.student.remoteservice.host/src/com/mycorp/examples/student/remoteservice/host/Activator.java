94accd7573b96a46c7c731e54a955611ffd20657
slewis <slewis@composent.com>
2015-08-28 23:06:48 -0700

    Initial checkin of com.mycorp.examples.student.remoteservice.host

---
+++ examples/com.mycorp.examples.student.remoteservice.host/src/com/mycorp/examples/student/remoteservice/host/Activator.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+* Copyright (c) 2015 Composent, Inc. and others. All rights reserved. This
+* program and the accompanying materials are made available under the terms of
+* the Eclipse Public License v1.0 which accompanies this distribution, and is
+* available at http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   Composent, Inc. - initial API and implementation
+******************************************************************************/
+package com.mycorp.examples.student.remoteservice.host;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.mycorp.examples.student.StudentService;
+
+public class Activator implements BundleActivator {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void start(BundleContext context) throws Exception {
+
+		Dictionary props = new Hashtable();
+		props.put("service.exported.interfaces", "*");
+		props.put("service.exported.configs", "ecf.container.server.jersey");
+		context.registerService(StudentService.class, new StudentResource(), props);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
