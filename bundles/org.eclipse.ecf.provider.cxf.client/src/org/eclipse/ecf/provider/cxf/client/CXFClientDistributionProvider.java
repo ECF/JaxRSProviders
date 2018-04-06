ae2a8b48dbcdb4819c3467655280f80bae0e3150
slewis <slewis@composent.com>
2018-04-05 17:32:12 -0700

    Added support for CXF's JaxRS implementation as distribution provider.

--- bundles/org.eclipse.ecf.provider.cxf.client/src/org/eclipse/ecf/provider/cxf/client/CXFClientDistributionProvider.java
+++ bundles/org.eclipse.ecf.provider.cxf.client/src/org/eclipse/ecf/provider/cxf/client/CXFClientDistributionProvider.java
@@ -10,12 +10,20 @@
 ******************************************************************************/
 package org.eclipse.ecf.provider.cxf.client;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import javax.ws.rs.RuntimeType;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Configurable;
 import javax.ws.rs.core.Configuration;
+import javax.ws.rs.core.Feature;
 import javax.ws.rs.ext.ContextResolver;
 
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
@@ -30,17 +38,151 @@ import org.eclipse.ecf.remoteservice.client.RemoteServiceClientRegistration;
 import org.eclipse.ecf.remoteservice.provider.IRemoteServiceDistributionProvider;
 import org.osgi.service.component.annotations.Component;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+
 @Component(immediate=true,service=IRemoteServiceDistributionProvider.class)
 public class CXFClientDistributionProvider extends JaxRSDistributionProvider {
 
 	public static final String CLIENT_PROVIDER_NAME = "ecf.jaxrs.cxf.client";
 	public static final String SERVER_PROVIDER_NAME = "ecf.jaxrs.cxf.server";
 
+	public class CXFClientConfiguration implements Configuration {
+
+		private List<Object> extensions;
+		private Map<String,Object> properties;
+		
+		public CXFClientConfiguration(List<Object> extensions, Map<String,Object> props) {
+			this.extensions = extensions;
+			this.properties = props;
+		}
+		@Override
+		public Set<Class<?>> getClasses() {
+			return null;
+		}
+
+		@Override
+		public Map<Class<?>, Integer> getContracts(Class<?> arg0) {
+			return null;
+		}
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		@Override
+		public Set<Object> getInstances() {
+			return new HashSet(extensions);
+		}
+
+		public List<Object> getExtensions() {
+			return extensions;
+		}
+		
+		@Override
+		public Map<String, Object> getProperties() {
+			return properties;
+		}
+
+		@Override
+		public Object getProperty(String arg0) {
+			return properties.get(arg0);
+		}
+
+		@Override
+		public Collection<String> getPropertyNames() {
+			return properties.keySet();
+		}
+
+		@Override
+		public RuntimeType getRuntimeType() {
+			return null;
+		}
+
+		@Override
+		public boolean isEnabled(Feature arg0) {
+			return false;
+		}
+
+		@Override
+		public boolean isEnabled(Class<? extends Feature> arg0) {
+			return false;
+		}
+
+		@Override
+		public boolean isRegistered(Object arg0) {
+			return extensions.contains(arg0);
+		}
+
+		@Override
+		public boolean isRegistered(Class<?> arg0) {
+			return false;
+		}
+		
+	}
+	@SuppressWarnings("rawtypes")
+	public class CXFClientConfigurable implements Configurable {
+
+		private Map<String,Object> properties = new HashMap<String,Object>();
+		private List<Object> extensions = new ArrayList<Object>();
+		
+		@Override
+		public Configuration getConfiguration() {
+			return new CXFClientConfiguration(extensions,properties);
+		}
+
+		@Override
+		public Configurable property(String arg0, Object arg1) {
+			properties.put(arg0, arg1);
+			return this;
+		}
+
+		@Override
+		public Configurable register(Class arg0) {
+			return null;
+		}
+
+		@Override
+		public Configurable register(Object arg0) {
+			this.extensions.add(arg0);
+			return this;
+		}
+
+		@Override
+		public Configurable register(Class arg0, int arg1) {
+			return null;
+		}
+
+		@Override
+		public Configurable register(Class arg0, Class... arg1) {
+			return null;
+		}
+
+		@Override
+		public Configurable register(Class arg0, Map arg1) {
+			return null;
+		}
+
+		@Override
+		public Configurable register(Object arg0, int arg1) {
+			this.extensions.add(arg0);
+			return this;
+		}
+
+		@Override
+		public Configurable register(Object arg0, Class... arg1) {
+			this.extensions.add(arg0);
+			return this;
+		}
+
+		@Override
+		public Configurable register(Object arg0, Map arg1) {
+			this.extensions.add(arg0);
+			return this;
+		}
+		
+	}
 	public CXFClientDistributionProvider() {
 		super(CLIENT_PROVIDER_NAME, new JaxRSContainerInstantiator(SERVER_PROVIDER_NAME, CLIENT_PROVIDER_NAME) {
 			@Override
 			public IContainer createInstance(ContainerTypeDescription description, Map<String, ?> parameters,
-					Configuration configuration) {
+					final Configuration configuration) {
 				return new JaxRSClientContainer() {
 					@Override
 					protected IRemoteService createRemoteService(RemoteServiceClientRegistration registration) {
@@ -50,7 +192,10 @@ public class CXFClientDistributionProvider extends JaxRSDistributionProvider {
 							protected Object createJaxRSProxy(ClassLoader cl,
 									@SuppressWarnings("rawtypes") Class interfaceClass, WebTarget webTarget)
 									throws ECFException {
-								return JAXRSClientFactory.create(getConnectedTarget(), interfaceClass);
+								List<Object> providers = new ArrayList<Object>();
+								providers.add(new JacksonJaxbJsonProvider());
+								providers.add(new ObjectMapperContextResolverComponent());
+								return JAXRSClientFactory.create(getConnectedTarget(), interfaceClass, ((CXFClientConfiguration) configuration).getExtensions());
 							}
 
 							@Override
@@ -73,7 +218,10 @@ public class CXFClientDistributionProvider extends JaxRSDistributionProvider {
 	@SuppressWarnings("rawtypes")
 	@Override
 	protected Configurable createConfigurable() {
-		return null;
+		CXFClientConfigurable configurable = new CXFClientConfigurable();
+		configurable.register(new JacksonJaxbJsonProvider());
+		configurable.register(new ObjectMapperContextResolverComponent());
+		return configurable;
 	}
 
 }
--- bundles/org.eclipse.ecf.provider.cxf.server/src/org/eclipse/ecf/provider/cxf/server/CXFServerDistributionProvider.java
+++ bundles/org.eclipse.ecf.provider.cxf.server/src/org/eclipse/ecf/provider/cxf/server/CXFServerDistributionProvider.java
@@ -10,16 +10,26 @@
 package org.eclipse.ecf.provider.cxf.server;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.ws.rs.RuntimeType;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Configurable;
 import javax.ws.rs.core.Configuration;
+import javax.ws.rs.core.Feature;
 
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet;
+import org.apache.cxf.jaxrs.utils.ResourceUtils;
 import org.eclipse.ecf.core.ContainerCreateException;
 import org.eclipse.ecf.core.ContainerTypeDescription;
 import org.eclipse.ecf.core.IContainer;
@@ -29,12 +39,175 @@ import org.eclipse.ecf.provider.jaxrs.server.JaxRSServerDistributionProvider;
 import org.eclipse.ecf.remoteservice.RSARemoteServiceContainerAdapter.RSARemoteServiceRegistration;
 import org.osgi.framework.BundleContext;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+
 public class CXFServerDistributionProvider extends JaxRSServerDistributionProvider {
 
 	public static final String SERVER_CONFIG_NAME = "ecf.jaxrs.cxf.server";
 
 	public static final String URI_PARAM = "uri";
 	public static final String URI_DEFAULT = "http://localhost:8080/cxf";
+
+	public class CXFServerConfiguration implements Configuration {
+
+		private List<Object> extensions;
+		private Map<String,Object> properties;
+		
+		public CXFServerConfiguration(List<Object> extensions, Map<String,Object> props) {
+			this.extensions = extensions;
+			this.properties = props;
+		}
+		@Override
+		public Set<Class<?>> getClasses() {
+			return null;
+		}
+
+		@Override
+		public Map<Class<?>, Integer> getContracts(Class<?> arg0) {
+			return null;
+		}
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		@Override
+		public Set<Object> getInstances() {
+			return new HashSet(extensions);
+		}
+
+		public List<Object> getExtensions() {
+			return extensions;
+		}
+		
+		@Override
+		public Map<String, Object> getProperties() {
+			return properties;
+		}
+
+		@Override
+		public Object getProperty(String arg0) {
+			return properties.get(arg0);
+		}
+
+		@Override
+		public Collection<String> getPropertyNames() {
+			return properties.keySet();
+		}
+
+		@Override
+		public RuntimeType getRuntimeType() {
+			return null;
+		}
+
+		@Override
+		public boolean isEnabled(Feature arg0) {
+			return false;
+		}
+
+		@Override
+		public boolean isEnabled(Class<? extends Feature> arg0) {
+			return false;
+		}
+
+		@Override
+		public boolean isRegistered(Object arg0) {
+			return extensions.contains(arg0);
+		}
+
+		@Override
+		public boolean isRegistered(Class<?> arg0) {
+			return false;
+		}
+		
+	}
+	@SuppressWarnings("rawtypes")
+	public class CXFServerConfigurable implements Configurable {
+
+		private Map<String,Object> properties = new HashMap<String,Object>();
+		private List<Object> extensions = new ArrayList<Object>();
+		
+		@Override
+		public Configuration getConfiguration() {
+			return new CXFServerConfiguration(extensions,properties);
+		}
+
+		@Override
+		public Configurable property(String arg0, Object arg1) {
+			properties.put(arg0, arg1);
+			return this;
+		}
+
+		@Override
+		public Configurable register(Class arg0) {
+			return null;
+		}
+
+		@Override
+		public Configurable register(Object arg0) {
+			this.extensions.add(arg0);
+			return this;
+		}
+
+		@Override
+		public Configurable register(Class arg0, int arg1) {
+			return null;
+		}
+
+		@Override
+		public Configurable register(Class arg0, Class... arg1) {
+			return null;
+		}
+
+		@Override
+		public Configurable register(Class arg0, Map arg1) {
+			return null;
+		}
+
+		@Override
+		public Configurable register(Object arg0, int arg1) {
+			this.extensions.add(arg0);
+			return this;
+		}
+
+		@Override
+		public Configurable register(Object arg0, Class... arg1) {
+			this.extensions.add(arg0);
+			return this;
+		}
+
+		@Override
+		public Configurable register(Object arg0, Map arg1) {
+			this.extensions.add(arg0);
+			return this;
+		}
+		
+	}
+	
+	public class DPCXFNonSpringJaxrsServlet extends CXFNonSpringJaxrsServlet {
+		
+		private static final long serialVersionUID = -2618572428261717260L;
+
+		private CXFServerConfiguration config;
+		
+		public DPCXFNonSpringJaxrsServlet(Application application, CXFServerConfiguration config) {
+			super(application);
+			this.config = config;
+		}
+
+		@Override
+		protected void createServerFromApplication(ServletConfig servletConfig) throws ServletException {
+	        Application app = getApplication();
+	        JAXRSServerFactoryBean bean = ResourceUtils.createApplication(
+	                                          app,
+	                                          isIgnoreApplicationPath(servletConfig),
+	                                          getStaticSubResolutionValue(servletConfig),
+	                                          isAppResourceLifecycleASingleton(app, servletConfig),
+	                                          getBus());
+	        bean.setBus(getBus());
+	        bean.setApplication(getApplication());
+	        bean.setProviders(config.getExtensions());
+	        bean.create();
+		}
+	}
+	
 
 	public CXFServerDistributionProvider(final BundleContext context) {
 		super();
@@ -53,7 +226,7 @@ public class CXFServerDistributionProvider extends JaxRSServerDistributionProvid
 
 			@Override
 			public IContainer createInstance(ContainerTypeDescription description, Map<String, ?> parameters,
-					Configuration configuration) throws ContainerCreateException {
+					final Configuration configuration) throws ContainerCreateException {
 				String uriStr = getParameterValue(parameters, URI_PARAM, URI_DEFAULT);
 				URI uri = null;
 				try {
@@ -65,7 +238,7 @@ public class CXFServerDistributionProvider extends JaxRSServerDistributionProvid
 				return new JaxRSServerContainer(context, uri) {
 					@Override
 					protected Servlet createServlet(final RSARemoteServiceRegistration registration) {
-						return new CXFNonSpringJaxrsServlet(new Application() {
+						return new DPCXFNonSpringJaxrsServlet(new Application() {
 							@Override
 							public Set<Class<?>> getClasses() {
 								Set<Class<?>> results = new HashSet<Class<?>>();
@@ -73,7 +246,7 @@ public class CXFServerDistributionProvider extends JaxRSServerDistributionProvid
 								return results;
 							}
 							
-						});
+						}, (CXFServerConfiguration) configuration);
 					}
 
 					@Override
@@ -88,17 +261,15 @@ public class CXFServerDistributionProvider extends JaxRSServerDistributionProvid
 		});
 		setDescription("CXF Jax-RS Distribution Provider");
 		setServer(true);
-//		JacksonJaxbJsonProvider p = new JacksonJaxbJsonProvider();
-//		addJaxRSComponent(new ObjectMapperContextResolverComponent(), ContextResolver.class);
-		//addJaxRSComponent(new JAXBElementProvider(),)
-//		addJaxRSComponent(p , MessageBodyWriter.class);
-//		addJaxRSComponent(p , MessageBodyReader.class);
 	}
 
 	@SuppressWarnings("rawtypes")
 	@Override
 	protected Configurable createConfigurable() {
-		return null;
+		CXFServerConfigurable configurable = new CXFServerConfigurable();
+		configurable.register(new JacksonJaxbJsonProvider());
+		configurable.register(new ObjectMapperContextResolverComponent());
+		return configurable;
 	}
 
 }
