0615804be21bc9a2ded0d04dd37aa11c08c8837e
slewis <slewis@composent.com>
2019-07-18 20:41:20 -0700

    Added org.eclipse.ecf.provider.jersey.ext.example provider

---
+++ examples/org.eclipse.ecf.provider.jersey.ext.example/src/org/eclipse/ecf/provider/internal/jersey/ext/example/Activator.java
@@ -0,0 +1,63 @@
+package org.eclipse.ecf.provider.internal.jersey.ext.example;
+
+import java.net.URI;
+import java.util.Map;
+
+import javax.ws.rs.core.Configurable;
+import javax.ws.rs.core.Configuration;
+
+import org.eclipse.ecf.core.ContainerCreateException;
+import org.eclipse.ecf.core.ContainerTypeDescription;
+import org.eclipse.ecf.core.IContainer;
+import org.eclipse.ecf.provider.jaxrs.server.JaxRSServerContainerInstantiator;
+import org.eclipse.ecf.provider.jaxrs.server.JaxRSServerDistributionProvider;
+import org.eclipse.ecf.remoteservice.provider.IRemoteServiceDistributionProvider;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	public static final String EXT_JERSEY_SERVER_CONFIG = "ecf.jaxrs.jersey.server.ext";
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// This sets up the 'ecf.jaxrs.jersey.server.ext' remote service provider
+		context.registerService(IRemoteServiceDistributionProvider.class, new JaxRSServerDistributionProvider(
+				EXT_JERSEY_SERVER_CONFIG, new JaxRSServerContainerInstantiator(EXT_JERSEY_SERVER_CONFIG) {
+					@Override
+					public IContainer createInstance(ContainerTypeDescription description, Map<String, ?> parameters,
+							Configuration configuration) throws ContainerCreateException {
+						URI uri = getUri(parameters, EXT_JERSEY_SERVER_CONFIG);
+						checkOSGIIntents(description, uri, parameters);
+						return new ExtJerseyServerContainer(createJaxRSID(uri), context,
+								(ResourceConfig) configuration);
+					}
+
+					@Override
+					protected boolean supportsOSGIConfidentialIntent(ContainerTypeDescription description) {
+						return true;
+					}
+
+					@Override
+					protected boolean supportsOSGIPrivateIntent(ContainerTypeDescription description) {
+						return true;
+					}
+
+					@Override
+					protected boolean supportsOSGIAsyncIntent(ContainerTypeDescription description) {
+						return true;
+					}
+				}, "Extended Jersey Server Remote Service Provider", true) {
+			@Override
+			protected Configurable<?> createConfigurable() {
+				return new ResourceConfig();
+			}
+		}, null);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
---
+++ examples/org.eclipse.ecf.provider.jersey.ext.example/src/org/eclipse/ecf/provider/internal/jersey/ext/example/ExtJerseyServerContainer.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+* Copyright (c) 2019 Composent, Inc. All rights reserved. This
+* program and the accompanying materials are made available under the terms of
+* the Eclipse Public License v1.0 which accompanies this distribution, and is
+* available at http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   Composent, Inc. - initial API and implementation
+******************************************************************************/
+package org.eclipse.ecf.provider.internal.jersey.ext.example;
+
+import org.eclipse.ecf.core.identity.URIID;
+import org.eclipse.ecf.provider.jaxrs.JaxRSContainerInstantiator;
+import org.eclipse.ecf.provider.jersey.server.JerseyServerContainer;
+import org.eclipse.ecf.remoteservice.RSARemoteServiceContainerAdapter.RSARemoteServiceRegistration;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.osgi.framework.BundleContext;
+
+public class ExtJerseyServerContainer extends JerseyServerContainer {
+
+	public ExtJerseyServerContainer(URIID containerID, BundleContext context, ResourceConfig configuration) {
+		super(containerID, context, configuration, JaxRSContainerInstantiator.JACKSON_DEFAULT_PRIORITY,
+				JerseyServerContainer.BINDING_DEFAULT_PRIORITY);
+	}
+
+	@Override
+	protected String getServletAlias(RSARemoteServiceRegistration reg) {
+		// Here's what all this was for...we return an empty string
+		return "";
+		// rather than the reg.getServiceId() value as string. This
+		// is what the superclass does:
+		// return String.valueOf(reg.getServiceId());
+	}
+}
