e7ed8e8e79c4e5f82968ed942aad5bce77746644
slewis <slewis@composent.com>
2019-11-05 16:08:03 -0800

    Update of karaf versions

--- bundles/org.eclipse.ecf.provider.cxf.server/src/org/eclipse/ecf/provider/cxf/server/Activator.java
+++ bundles/org.eclipse.ecf.provider.cxf.server/src/org/eclipse/ecf/provider/cxf/server/Activator.java
@@ -9,7 +9,9 @@
 ******************************************************************************/
 package org.eclipse.ecf.provider.cxf.server;
 
+import org.eclipse.ecf.core.util.BundleStarter;
 import org.eclipse.ecf.remoteservice.provider.IRemoteServiceDistributionProvider;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -19,6 +21,11 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext context) throws Exception {
 		System.setProperty("javax.ws.rs.ext.RuntimeDelegate", "org.apache.cxf.jaxrs.impl.RuntimeDelegateImpl");
 		System.setProperty("org.apache.cxf.stax.allowInsecureParser", "1");
+		BundleStarter.startDependents(context,
+				new String[] { "org.apache.cxf.cxf-core", "org.apache.cxf.cxf-rt-frontend-jaxrs",
+						"org.apache.cxf.cxf-rt-transports-http", "org.eclipse.ecf.provider.jaxrs",
+						"org.eclipse.ecf.provider.jaxrs.server" },
+				Bundle.RESOLVED | Bundle.STARTING);
 		// Register CXF Server distribution provider
 		context.registerService(IRemoteServiceDistributionProvider.class, new CXFServerDistributionProvider(context),
 				null);
