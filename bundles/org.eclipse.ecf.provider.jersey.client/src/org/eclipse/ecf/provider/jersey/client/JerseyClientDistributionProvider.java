32742f4c268a673e3f47e84611ad8cf449f95d0d
Patrick Paulin <patrick@modumind.com>
2020-07-14 13:20:56 -0500

    Restore JAX-RS component bindings.

--- bundles/org.eclipse.ecf.provider.jersey.client/src/org/eclipse/ecf/provider/jersey/client/JerseyClientDistributionProvider.java
+++ bundles/org.eclipse.ecf.provider.jersey.client/src/org/eclipse/ecf/provider/jersey/client/JerseyClientDistributionProvider.java
@@ -11,8 +11,17 @@ package org.eclipse.ecf.provider.jersey.client;
 
 import java.util.Map;
 
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.client.ClientResponseFilter;
 import javax.ws.rs.core.Configurable;
 import javax.ws.rs.core.Configuration;
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.ReaderInterceptor;
+import javax.ws.rs.ext.WriterInterceptor;
 
 import org.eclipse.ecf.core.ContainerTypeDescription;
 import org.eclipse.ecf.core.IContainer;
@@ -22,6 +31,8 @@ import org.eclipse.ecf.provider.jaxrs.client.JaxRSClientDistributionProvider;
 import org.eclipse.ecf.remoteservice.provider.IRemoteServiceDistributionProvider;
 import org.glassfish.jersey.client.ClientConfig;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
 
 @Component(service = IRemoteServiceDistributionProvider.class)
 public class JerseyClientDistributionProvider extends JaxRSClientDistributionProvider {
@@ -48,4 +59,97 @@ public class JerseyClientDistributionProvider extends JaxRSClientDistributionPro
 		return new ClientConfig();
 	}
 
+	@SuppressWarnings("rawtypes")
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE) 
+	protected void bindMessageBodyWriter(MessageBodyWriter instance, Map serviceProps) {
+		bindJaxComponent(instance, serviceProps);
+	}
+
+	@SuppressWarnings("rawtypes")
+	protected void unbindMessageBodyWriter(MessageBodyWriter instance) {
+		removeJaxComponent(instance);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE)
+	protected void bindMessageBodyReader(MessageBodyReader instance, Map serviceProps) {
+		bindJaxComponent(instance, serviceProps);
+	}
+
+	@SuppressWarnings("rawtypes")
+	protected void unbindMessageBodyReader(MessageBodyReader instance) {
+		removeJaxComponent(instance);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE)
+	protected void bindContextResolver(ContextResolver instance, Map serviceProps) {
+		bindJaxComponent(instance, serviceProps);
+	}
+
+	@SuppressWarnings("rawtypes")
+	protected void unbindContextResolver(ContextResolver instance) {
+		removeJaxComponent(instance);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE)
+	protected void bindExceptionMapper(ExceptionMapper instance, Map serviceProps) {
+		bindJaxComponent(instance, serviceProps);
+	}
+
+	@SuppressWarnings("rawtypes")
+	protected void unbindExceptionMapper(ExceptionMapper instance) {
+		removeJaxComponent(instance);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE)
+	protected void bindFeature(Feature instance, Map serviceProps) {
+		bindJaxComponent(instance, serviceProps);
+	}
+
+	protected void unbindFeature(Feature instance) {
+		removeJaxComponent(instance);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE)
+	protected void bindReaderInterceptor(ReaderInterceptor instance, Map serviceProps) {
+		bindJaxComponent(instance, serviceProps);
+	}
+
+	protected void unbindReaderInterceptor(ReaderInterceptor instance) {
+		removeJaxComponent(instance);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE)
+	protected void bindWriterInterceptor(WriterInterceptor instance, Map serviceProps) {
+		bindJaxComponent(instance, serviceProps);
+	}
+
+	protected void unbindWriterInterceptor(WriterInterceptor instance) {
+		removeJaxComponent(instance);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE)
+	protected void bindClientRequestFilter(ClientRequestFilter instance, Map serviceProps) {
+		super.bindClientRequestFilter(instance, serviceProps);
+	}
+
+	protected void unbindClientRequestFilter(ClientRequestFilter instance) {
+		super.unbindClientRequestFilter(instance);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE)
+	protected void bindClientResponseFilter(ClientResponseFilter instance, Map serviceProps) {
+		super.bindClientResponseFilter(instance, serviceProps);
+	}
+
+	protected void unbindClientResponseFilter(ClientResponseFilter instance) {
+		super.unbindClientResponseFilter(instance);
+	}
 }
